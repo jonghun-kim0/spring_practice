394701d2536353497084c60189556c1c14d0ea4e
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-07-03 14:49:56 +0900

    refactor: 테스트 코드 given,when,then 패턴으로 구분.

--- src/test/java/com/example/springprepare/testsample/CalculatorTest.java
+++ src/test/java/com/example/springprepare/testsample/CalculatorTest.java
@@ -1,26 +1,78 @@
 package com.example.springprepare.testsample;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+@Nested
+@DisplayName("계산기 테스트")
 
 class CalculatorTest {
     @Test
-    @DisplayName("더하기 테스트")
+    @DisplayName("곱하기 테스트")
     void test1() {
+        //given (주어진 값.)
         Calculator calculator = new Calculator();
-        Double result = calculator.operate(4,"+",3);
+        int num1 = 8;
+        int num2 = 2;
+        String op = "*";
+
+        //when (테스트 실행.)
+        Double result = calculator.operate(num1,op,num2);
+
+        //then (결과.)
         System.out.println("result = " + result);
-        Assertions.assertEquals(7, result);
+        Assertions.assertEquals(16, result);
         }
 
     @Test
     @DisplayName("나누기 테스트")
     void test2() {
+        //given
         Calculator calculator = new Calculator();
-        Double result = calculator.operate(8,"/",2);
+        int num1 = 8;
+        int num2 = 2;
+        String op = "/";
+
+        //when (테스트 실행.)
+        Double result = calculator.operate(num1,op,num2);
+
+        //then (결과.)
         System.out.println("result = " + result);
         Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    @DisplayName("더하기 테스트")
+    void test3() {
+        //given
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 2;
+        String op = "+";
+
+        //when (테스트 실행.)
+        Double result = calculator.operate(num1,op,num2);
+
+        //then (결과.)
+        System.out.println("result = " + result);
+        Assertions.assertEquals(10, result);
+    }
+
+    @Test
+    @DisplayName("빼기 테스트")
+    void test4() {
+        //given
+        Calculator calculator = new Calculator();
+        int num1 = 8;
+        int num2 = 2;
+        String op = "-";
+
+        //when (테스트 실행.)
+        Double result = calculator.operate(num1,op,num2);
+
+        //then (결과.)
+        System.out.println("result = " + result);
+        Assertions.assertEquals(6, result);
     }
 }
