c1ec0557fb2210686f91bc6e6da20730551bcd3b
jonghun_kim <kjonghun0828@g.hongik.ac.kr>
2025-07-04 10:36:01 +0900

    feat : Lombok 에너테이션 활용.
    * RequiredArgsConstructor final 제어자가 붙은 생성자

--- src/main/java/com/example/springprepare/domain/Memo.java
+++ src/main/java/com/example/springprepare/domain/Memo.java
@@ -4,13 +4,13 @@ import lombok.*;
 
 //@Getter
 //@Setter //값 변경
-@NoArgsConstructor //기본 생성자 생성 에너테이션
-@AllArgsConstructor
-//@RequiredArgsConstructor
+//@NoArgsConstructor //기본 생성자 생성 에너테이션
+//@AllArgsConstructor
+@RequiredArgsConstructor
 
 public class Memo {
-    private String username;
+    private final String username;
     private String content;
     private String description;
-    private String password;
+    private final String password;
 }
